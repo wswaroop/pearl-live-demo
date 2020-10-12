1d58d8b5ebbcfe1a309f75217f260edec4398642
Swaroop Wakade <swakade@newstore.com>
2020-10-12 16:18:53 +0530

    first commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Calculator {
+    public static int add(int first, int second) {
+        return first + second;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import calculator.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void TestAdd(){
+        int sum = Calculator.add(1,2);
+
+    }
+}
